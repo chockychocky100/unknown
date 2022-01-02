071d99f3985895ee7c23496231a14326f52c441d
chockychocky100 <92289181+chockychocky100@users.noreply.github.com>
2022-01-02 17:55:00 +0530

    Update k_nearest_neighbor.py

--- k_nearest_neighbor.py
+++ k_nearest_neighbor.py
@@ -3,7 +3,6 @@ from sklearn import datasets
 iris = datasets.load_iris()
 x = iris.data
 y = iris.target
-print("S. Dhivya Dharani S 1817114")
 print("Iris Data set loaded..")
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.1)
 print("Dataset is split into training and testing...") 
